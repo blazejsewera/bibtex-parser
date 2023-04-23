596b18cabbf7cca247faa4be27849a3c5afa3326
Blazej Sewera <code@sewera.dev>
2023-04-23 02:40:07 +0200

    test: write a naive test for parsing

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,39 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[derive(PartialEq, Debug)]
+struct Book {
+    title: String,
+    edition: i32,
+}
+
+fn parse_bibtex(_entry: String) -> Book {
+    Book {
+        title: String::from("Extreme Programming Explained: Embrace Change"),
+        edition: 2,
+    }
+}
+
 #[test]
-fn t() {
-    assert_eq!(1, 2);
-    assert_eq!(1, 1);
+fn parse_bibtex_entry() {
+    // given
+    let entry = String::from(
+        r#"
+        @book{beck-2004,
+          title     = {Extreme Programming Explained: Embrace Change},
+          edition   = {2},
+          isbn      = {978-0-13-405199-4},
+          series    = {{XP} Series},
+          pagetotal = {189},
+          publisher = {Addison-Wesley Professional},
+          author    = {Beck, Kent and Andres, Cynthia},
+          date      = {2004},
+        }"#,
+    );
+    let expected = Book {
+        title: String::from("Extreme Programming Explained: Embrace Change"),
+        edition: 2,
+    };
+    let parsed = parse_bibtex(entry);
+    assert_eq!(expected, parsed)
 }
